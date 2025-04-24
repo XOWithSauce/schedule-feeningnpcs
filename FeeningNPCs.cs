9db37ac222d9cfa7145b89b0fea2972b0641c18c
JP <122294168+XOWithSauce@users.noreply.github.com>
2025-04-24 20:15:50 +0000

    v1.2.3

--- FeeningNPCs.cs
+++ FeeningNPCs.cs
@@ -7,6 +7,13 @@ using ScheduleOne.Persistence;
 using MelonLoader.Utils;
 using ScheduleOne.VoiceOver;
 using ScheduleOne.AvatarFramework.Equipping;
+using ScheduleOne.Tools;
+using HarmonyLib;
+using ScheduleOne.DevUtilities;
+using ScheduleOne.Money;
+using ScheduleOne.ItemFramework;
+using ScheduleOne.UI.Handover;
+using ScheduleOne.Quests;
 
 [assembly: MelonInfo(typeof(FeeningNPCs.FeeningNPCs), FeeningNPCs.BuildInfo.Name, FeeningNPCs.BuildInfo.Version, FeeningNPCs.BuildInfo.Author, FeeningNPCs.BuildInfo.DownloadLink)]
 [assembly: MelonColor()]
@@ -21,7 +28,7 @@ namespace FeeningNPCs
         public const string Description = "Your customers need more... and more.... AND MORE!!! HELP ME BRO YOU GOT SOME MORE??!!";
         public const string Author = "XOWithSauce";
         public const string Company = null;
-        public const string Version = "1.2.2";
+        public const string Version = "1.2.3";
         public const string DownloadLink = null;
     }
 
@@ -35,6 +42,8 @@ namespace FeeningNPCs
         public int feeningRadius = 30; // max distance away from player
         public bool randomEffects = true; // when true rolls miscellanious actions
         public bool increaseEconomy = true; // when true adds more spending habits
+        public int economyMultiplier = 10; // 1 - 10 multiplier (1 small economy growth, 10 max economy growth)
+        public bool ignoreExistingDeals = true; // when false, Customer that has deal cant become a feenns
     }
 
 
@@ -83,11 +92,11 @@ namespace FeeningNPCs
 
     public class FeeningNPCs : MelonMod
     {
-        Customer[] cmers;
+        public static Customer[] cmers;
         public static List<object> coros = new();
-        HashSet<Customer> feens = new();
+        public static HashSet<Customer> feens = new();
         public static bool registered = false;
-        private ModConfig currentConfig;
+        public static ModConfig currentConfig;
 
         public override void OnApplicationStart()
         {
@@ -104,27 +113,14 @@ namespace FeeningNPCs
                 }
             }
             else
-            {
-                if (LoadManager.Instance != null && registered)
-                {
-                    LoadManager.Instance.onLoadComplete.RemoveListener(OnLoadCompleteCb);
-                }
-                registered = false;
-
-                foreach (object coro in coros)
-                {
-                    MelonCoroutines.Stop(coro);
-                }
-                coros.Clear();
-                feens.Clear();
-            }
+                LoadManager.Instance.onLoadComplete.RemoveListener(OnLoadCompleteCb);
         }
 
         private void OnLoadCompleteCb()
         {
             if (registered) return;
             currentConfig = ConfigLoader.Load();
-            this.cmers = UnityEngine.Object.FindObjectsOfType<Customer>(true);
+            cmers = UnityEngine.Object.FindObjectsOfType<Customer>(true);
             coros.Add(MelonCoroutines.Start(this.ChangeBehv()));
             coros.Add(MelonCoroutines.Start(this.ClearFeens()));
             registered = true;
@@ -142,60 +138,172 @@ namespace FeeningNPCs
             }
         }
 
+        #region Harmony Patch Exit to Menu to break coros
+        [HarmonyPatch(typeof(ExitToMenu), "Exit")]
+        public static class Tools_ExitToMenu_Patch
+        {
+            public static bool Prefix(ExitToMenu __instance)
+            {
+                //MelonLogger.Msg("Pre-Exit Task");
+                registered = false;
+                foreach (object coro in coros)
+                {
+                    if (coro != null)
+                        MelonCoroutines.Stop(coro);
+                }
+                coros.Clear();
+                feens.Clear();
+                return true;
+            }
+        }
+        #endregion
+
+        #region Harmony Patch Process Handover
+        [HarmonyPatch(typeof(Customer), "ProcessHandover")]
+        public static class Customer_ProcessHandover_Patch
+        {
+            public static bool Prefix(Customer __instance, HandoverScreen.EHandoverOutcome outcome, Contract contract, List<ItemInstance> items, bool handoverByPlayer, bool giveBonuses = true)
+            {
+                //MelonLogger.Msg("ProcessHandover Customer Postfix");
+                coros.Add(MelonCoroutines.Start(PreProcessHandover(__instance, handoverByPlayer)));
+                return true;
+            }
+        }
+
+        public static IEnumerator PreProcessHandover(Customer __instance, bool handoverByPlayer)
+        {
+            if (!currentConfig.increaseEconomy) yield break;
+
+            // Player Average Economy
+            float lastNetWNorm = Mathf.Clamp01(MoneyManager.Instance.LastCalculatedNetworth / 10000000f);
+            float lifetimeNorm = Mathf.Clamp01(MoneyManager.Instance.LifetimeEarnings / 10000000f);
+            float cashBNorm = Mathf.Clamp01(MoneyManager.Instance.cashBalance / 10000000f);
+            float economyTotal = (lastNetWNorm + lifetimeNorm + cashBNorm) / 3f;
+            economyTotal = Mathf.Max(economyTotal, 0.01f);
+
+            // Validate economy multiplier
+            int currMult = 1;
+            if (currentConfig.economyMultiplier >= 1 && currentConfig.economyMultiplier <= 10)
+                currMult = currentConfig.economyMultiplier;
+
+            // Customer Economy
+            float weekSpend = __instance.CustomerData.MaxWeeklySpend;
+            float minWeekSpend = __instance.CustomerData.MinWeeklySpend;
+
+            float instSpendNorm = Mathf.Clamp01(weekSpend / 10000000f);
+            if (economyTotal <= instSpendNorm) yield break;
+
+            float diff = economyTotal - instSpendNorm; // max ~0.999 if at 10mil on all 
+
+            float target = weekSpend + ((weekSpend * currMult) * diff); // max = weekSpend * mult * 1.9999
+            if (target >= 500000f * currMult)
+                target = 500000f * currMult; // Cap at 5 mil at full mult
+
+            float targetMin = minWeekSpend + ((minWeekSpend * currMult) * diff); // max = minWeekSpend * mult * 1.9999
+            if (targetMin >= 166666f * currMult)
+                targetMin = 166666f * currMult; // Cap at 1.666mil at full mult
+
+            float adjustmentFactorMax = diff * (currMult * 0.1f);
+            float resultMax = Mathf.Lerp(
+                weekSpend,
+                target,
+                adjustmentFactorMax
+            );
+            resultMax = Mathf.Round(resultMax);
+            if (resultMax <= 5000000f)
+                __instance.CustomerData.MaxWeeklySpend = resultMax;
+
+            float adjustmentFactorMin = diff * (currMult * 0.03f);
+            float resultMin = Mathf.Lerp(
+                minWeekSpend,
+                targetMin,
+                adjustmentFactorMin
+            );
+            resultMin = Mathf.Round(resultMin);
+            if (resultMin <= 1666666f)
+                __instance.CustomerData.MinWeeklySpend = resultMin;
+
+            //MelonLogger.Msg($"EconomyTotal: {economyTotal}");
+            //MelonLogger.Msg($"MaxSpend ( {weekSpend} -> {__instance.CustomerData.MaxWeeklySpend} ) +{__instance.CustomerData.MaxWeeklySpend-weekSpend}");
+            //MelonLogger.Msg($"MinSpend ( {minWeekSpend} -> {__instance.CustomerData.MinWeeklySpend} ) +{__instance.CustomerData.MinWeeklySpend - minWeekSpend}");
+            //MelonLogger.Msg($"Adjustment Factor: {adjustmentFactorMin}/{adjustmentFactorMax}");
+
+            yield return null;
+        }
+
+
+        #endregion
+
         #region Random effect coros
-        public static IEnumerator ChangeCustomerEconomy(Customer c)
+        private IEnumerator ChangeCustomerEconomy(Customer c)
         {
             int roll = UnityEngine.Random.Range(0, 4);
+            // Validate economy multiplier
+            int currMult = 1;
+            if (currentConfig.economyMultiplier >= 1 && currentConfig.economyMultiplier <= 10)
+                currMult = currentConfig.economyMultiplier;
+
             switch (roll)
             {
                 // Orders count
-                case 0: // Increase MinOrdersPerWeek by 1
+                case 0: // Increase MinOrdersPerWeek by 1-3
                     {
-                        int baseMax = 12;
+                        int increment = Mathf.Clamp(Mathf.CeilToInt(currMult / 4f), 1, 3);
+                        int baseMax = 4 * currMult;
                         int current = c.CustomerData.MinOrdersPerWeek;
-                        if (c.CustomerData.MinOrdersPerWeek >= baseMax) break;
-                        c.CustomerData.MinOrdersPerWeek = Mathf.Max(baseMax, current + 1);
+                        int max = c.CustomerData.MaxOrdersPerWeek;
+                        int result = current + increment;
+                        if (current >= baseMax || result >= max) break;
+                        c.CustomerData.MinOrdersPerWeek = Mathf.Max(baseMax, result);
                         //MelonLogger.Msg("MinOrdersPerWeek: " + c.CustomerData.MinOrdersPerWeek);
                         break;
                     }
 
-                case 1: // Increase MaxOrdersPerWeek by 1
+                case 1: // Increase MaxOrdersPerWeek by 1-3
                     {
-                        int baseMax = 24;
+                        int increment = Mathf.Clamp(Mathf.CeilToInt(currMult / 4f), 1, 3);
+                        int baseMax = 7 * currMult;
                         int current = c.CustomerData.MaxOrdersPerWeek;
-                        if (c.CustomerData.MaxOrdersPerWeek >= baseMax) break;
-                        c.CustomerData.MaxOrdersPerWeek = Mathf.Max(baseMax, current + 1);
+                        int result = current + increment;
+                        if (current >= baseMax || result >= baseMax) break;
+                        c.CustomerData.MaxOrdersPerWeek = Mathf.Max(baseMax, result);
                         //MelonLogger.Msg("MaxOrdersPerWeek: " + c.CustomerData.MaxOrdersPerWeek);
                         break;
                     }
 
                 // Spending habits
-                case 2: // Increase MaxWeeklySpend by 1% - cap 30k
+                case 2: // Increase MaxWeeklySpend by 1-10%
                     {
-                        float baseMax = 30000f;
-                        if (c.CustomerData.MaxWeeklySpend >= baseMax) break;
-                        c.CustomerData.MaxWeeklySpend = Mathf.Max(baseMax, c.CustomerData.MaxWeeklySpend * 1.01f);
+                        // because max increment is 10% and dynamics cap at 5mil
+                        // we use 450 000 as base and at max multiplier 4 500 000 so that + 10% to that is always 5mil
+                        float baseMax = 450000f * currMult;
+                        float current = c.CustomerData.MaxWeeklySpend;
+                        float result = c.CustomerData.MaxWeeklySpend * (1.00f + (0.01f * currMult));
+                        if (current >= baseMax) break;
+                        c.CustomerData.MaxWeeklySpend = Mathf.Max(baseMax, result);
                         //MelonLogger.Msg("MaxWeeklySpend: " + c.CustomerData.MaxWeeklySpend);
                         break;
                     }
 
-
-                case 3: // Increase MinWeeklySpend by 1% - cap 10k
+                case 3: // Increase MinWeeklySpend by 1-10%
                     {
-                        float baseMax = 10000f;
-                        if (c.CustomerData.MinWeeklySpend >= baseMax) break;
-                        c.CustomerData.MinWeeklySpend = Mathf.Max(baseMax, c.CustomerData.MinWeeklySpend * 1.01f);
+                        // because max increment is 10% and dynamics cap around third of max week cap ~1.666mil
+                        // we use 150 000 as base and at max multiplier 1 500 000 so that + 10% to that is always max ~1.666mil
+                        float baseMax = 150000f * currMult;
+                        float current = c.CustomerData.MinWeeklySpend;
+                        float max = c.CustomerData.MaxWeeklySpend;
+                        float result = c.CustomerData.MinWeeklySpend * (1.00f + (0.01f * currMult));
+                        if (current >= baseMax || result >= max) break;
+                        c.CustomerData.MinWeeklySpend = Mathf.Max(baseMax, result);
                         //MelonLogger.Msg("MinWeeklySpend: " + c.CustomerData.MinWeeklySpend);
                         break;
                     }
-
-
             }
             yield return null;
         }
-        public static IEnumerator RandomFeenEffect(Customer c)
+        private IEnumerator RandomFeenEffect(Customer c)
         {
-            int roll = UnityEngine.Random.Range(0, 14);
+            int roll = UnityEngine.Random.Range(0, 8);
             switch (roll)
             {
                 case 0:
@@ -240,7 +348,6 @@ namespace FeeningNPCs
 
             yield return null;
         }
-
         private static IEnumerator NPCAttackEffect(Customer c)
         {
             //MelonLogger.Msg("NPC attack effect");
@@ -306,19 +413,22 @@ namespace FeeningNPCs
                 try
                 {
                     //MelonLogger.Msg("Evaluate Nearby Feens");
-                    Player[] players = UnityEngine.Object.FindObjectsOfType<Player>(true);
-                    if (players.Length == 0) { continue; }
-                    Player randomPlayer = players[UnityEngine.Random.Range(0, players.Length)];
+                    Player player = Player.Local;
                     List<Customer> nearbyCustomers = new();
 
                     foreach (Customer c in cmers)
                     {
-                        float dist = Vector3.Distance(c.NPC.transform.position, randomPlayer.transform.position);
+                        if (!currentConfig.ignoreExistingDeals && c.CurrentContract != null)
+                            continue;
+
+                        if (Vector3.Distance(c.NPC.transform.position, player.transform.position) > currentConfig.feeningRadius)
+                            continue;
+
                         string currentBehavior = "";
                         if (c.NPC.behaviour.activeBehaviour != null)
                             currentBehavior = c.NPC.behaviour.activeBehaviour.ToString();
 
-                        if (dist < currentConfig.feeningRadius && !currentBehavior.Contains("Request product from player") && !feens.Contains(c) && c.NPC.RelationData.Unlocked && !c.NPC.Health.IsKnockedOut && !c.NPC.Health.IsDead)
+                        if (!currentBehavior.Contains("Request product from player") && !feens.Contains(c) && c.NPC.RelationData.Unlocked && !c.NPC.Health.IsKnockedOut && !c.NPC.Health.IsDead && c.NPC.Movement.CanMove())
                         {
                             nearbyCustomers.Add(c);
                             if (nearbyCustomers.Count >= currentConfig.maxFeensStack)
@@ -329,7 +439,7 @@ namespace FeeningNPCs
                     if (nearbyCustomers.Count > 0)
                     {
                         //MelonLogger.Msg($"Nearby Feening: {nearbyCustomers.Count}");
-                        coros.Add(MelonCoroutines.Start(this.SetFeening(nearbyCustomers, randomPlayer)));
+                        coros.Add(MelonCoroutines.Start(this.SetFeening(nearbyCustomers, player)));
                     }
 
                 }
@@ -339,7 +449,7 @@ namespace FeeningNPCs
                 }
             }
         }
-        private IEnumerator SetFeening(List<Customer> cs, Player randomPlayer)
+        private IEnumerator SetFeening(List<Customer> cs, Player player)
         {
             foreach (Customer c in cs)
             {
@@ -349,17 +459,22 @@ namespace FeeningNPCs
                 if (c.NPC.isInBuilding)
                     c.NPC.ExitBuilding();
                 yield return new WaitForSeconds(0.5f);
-                if (!c.NPC.Movement.CanGetTo(randomPlayer.transform.position, proximityReq: currentConfig.feeningRadius))
+                if (!c.NPC.Movement.CanGetTo(player.transform.position, proximityReq: currentConfig.feeningRadius))
                     continue;
 
                 if (UnityEngine.Random.Range(0f, 1f) > 0.5f)
+                {
                     if (currentConfig.randomEffects)
                         coros.Add(MelonCoroutines.Start(RandomFeenEffect(c)));
-                    else
+                }
+                else
+                {
                     if (currentConfig.increaseEconomy)
                         coros.Add(MelonCoroutines.Start(ChangeCustomerEconomy(c)));
+                }
+                    
 
-                c.RequestProduct(randomPlayer);
+                c.RequestProduct(player);
                 feens.Add(c);
                 //MelonLogger.Msg("NearbyFeeningDone");
             }
